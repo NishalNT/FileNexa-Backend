9f0286dab6f66884fe392a1a3d104044cc3a31df
NishalNT <nishalnthingalaya1@gmail.com>
2024-06-29 09:21:53 +0530

    url

--- config/db.js
+++ config/db.js
@@ -1,18 +1,35 @@
 require("dotenv").config();
 
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-function connectDB() {
-    //Database connection
-    mongoose.connect(process.env.MONGODB_CONNECTION_URL)
-    .then((response) => {
-        console.log("Connected to database")
-    })
-    .catch((error) => {
-        console.log(error);
+// function connectDB() {
+//     mongoose.connect(process.env.MONGODB_CONNECTION_URL)
+//     .then((response) => {
+//         console.log("Connected to database")
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
+//     const connection = mongoose.connection;
+
+// }
+
+
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_CONNECTION_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds if no servers are available
+      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
     });
-    const connection = mongoose.connection;
-
-}
+    console.log('Connected to MongoDB Atlas');
+  } catch (error) {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  }
+};
 
 module.exports = connectDB;
+
